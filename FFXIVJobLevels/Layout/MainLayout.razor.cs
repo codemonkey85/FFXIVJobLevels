2c39c04af058f6a4080be47b35424b0764991c4c
Michael Bond <michael@bondcodes.com>
2024-03-25 17:21:17 -0400

    Initial commit of project files

---
+++ FFXIVJobLevels/Layout/MainLayout.razor.cs
@@ -0,0 +1,24 @@
+namespace FFXIVJobLevels.Layout;
+
+public partial class MainLayout
+{
+    private bool isDarkMode;
+    private MudThemeProvider? mudThemeProvider;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && mudThemeProvider is not null)
+        {
+            isDarkMode = await mudThemeProvider.GetSystemPreference();
+            await mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+
+    private Task OnSystemPreferenceChanged(bool newValue)
+    {
+        isDarkMode = newValue;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+}
